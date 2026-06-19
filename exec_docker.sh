bf79b61e3146c0701d356fa9baeb1d42d7dbc3af
Dowon Kim <kdw4537@github.com>
2026-06-19 19:09:22 +0900

    exec_docker

--- exec_docker.sh
+++ exec_docker.sh
@@ -1,7 +1,4 @@
 # docker exec -it <CONTAINER ID> bash -c "export DISPLAY=$DISPLAY && ln -s /tmp/.X11-unix /tmp/.X11-unix && bash"
-docker exec -it  \
-    0f3276e76fcc bash -c \
-    "export DISPLAY=$DISPLAY \
-    && ln -s /tmp/.X11-unix /tmp/.X11-unix \
-    && bash"
+. ./config.sh
 
+docker exec -it ${DOCKER_NAME} bash
--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-git clone git@github.com:kdw4537/workspace.git
+#git clone git@github.com:kdw4537/workspace.git
 
 . ./config.sh
 
 # docker run -v{volume} {host volume path}:/{docker volume path}
-docker run -it \
+docker run -dit \
 	--privileged \
 	--gpus all \
 	--env NVIDIA_DRIVER_CAPABILITIES=all \
@@ -16,4 +16,5 @@ docker run -it \
 	-v ${HOME_PATH}/.ssh:/root/.ssh \
 	-v /raid:/root/raid \
     -p 4537:4537 \
-	${IMAGE_NAME}:${IMAGE_TAG} /bin/bash
+	${IMAGE_NAME}:${IMAGE_TAG}
+
